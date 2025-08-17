8bc7fbb5c75a9bbbd4f5fdce8db416f20866dba5
Johan Gahnstrom <55078967+JohanGahnstrom@users.noreply.github.com>
2025-08-18 00:44:17 +0200

    Expose opencpn bridge utilities

--- VDR/opencpn_bridge/py/__init__.py
+++ VDR/opencpn_bridge/py/__init__.py
@@ -1,6 +1,4 @@
 from .ingest import ingest_dataset
-
-__all__ = ["ingest_dataset"]
 from .bridge import build_senc, query_tile_mvt
 
-__all__ = ["build_senc", "query_tile_mvt"]
+__all__ = ["ingest_dataset", "build_senc", "query_tile_mvt"]
